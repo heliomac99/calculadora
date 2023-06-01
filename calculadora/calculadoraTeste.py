e29b0d6c608b7db31396d59ea4aa6d41b11ef7ea
Hélio Martins <helio.1999.neto@gmail.com>
2023-06-01 19:27:01 -0300

    Adicionar arquivos de projeto.

---
+++ calculadora/calculadoraTeste.py
@@ -0,0 +1,16 @@
+import calculadora
+import pytest
+
+collect_ignore = ["setup.py"]
+
+def test_add():
+    assert calculadora.add(2, 3) == 5
+
+def test_subtract():
+    assert calculadora.subtract(5, 2) == 3
+
+def test_multiply():
+    assert calculadora.multiply(4, 5) == 20
+
+def test_divide():
+    assert calculadora.divide(10, 2) == 5
